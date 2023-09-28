8f8751ad43a22446cdc13a6e297831bc0b654a2b
Thitiphatx <thitiphat6391@gmail.com>
2023-09-28 16:14:01 +0700

    Disabled the buttons of methods that have not yet been developed.

--- src/navbar.js
+++ src/navbar.js
@@ -19,14 +19,14 @@ function HomeNav() {
             <Nav className="me-auto">
                 <Nav.Link href="/">Home</Nav.Link>
                 <NavDropdown title="Root of equation">
-                  <NavDropdown.Item href="#">Graphical method</NavDropdown.Item>
+                  <NavDropdown.Item href="#" disabled>Graphical method</NavDropdown.Item>
                   <NavDropdown.Item href="/bisection">Bisection</NavDropdown.Item>
-                  <NavDropdown.Item href="#">False Position</NavDropdown.Item>
-                  <NavDropdown.Item href="#">One point</NavDropdown.Item>
-                  <NavDropdown.Item href="#">Newton Raphson</NavDropdown.Item>
+                  <NavDropdown.Item href="#" disabled>False Position</NavDropdown.Item>
+                  <NavDropdown.Item href="#" disabled>One point</NavDropdown.Item>
+                  <NavDropdown.Item href="#" disabled>Newton Raphson</NavDropdown.Item>
                 </NavDropdown>
                 <NavDropdown title="Inter/Extra -polation">
-                <NavDropdown.Item href="/newton-div-diff">Newton's Divide-difference</NavDropdown.Item>
+                <NavDropdown.Item href="/newton-div-diff" disabled>Newton's Divide-difference</NavDropdown.Item>
                   <NavDropdown.Item href="/lagrange">Lagrange</NavDropdown.Item>
                 </NavDropdown>
             </Nav>
