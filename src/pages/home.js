110386c0d8cde2174aacd19585f893ae8fb762d8
Thitiphatx <thitiphat6391@gmail.com>
2023-10-05 22:10:44 +0700

    Remove test

--- src/pages/home.js
+++ src/pages/home.js
@@ -2,14 +2,12 @@ import React from "react";
 // Bootstrap
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col"
-import MatrixFieldForm from "./Interpolation/test";
 
 class Home extends React.Component {
     render() {
         return(
             <Row>
                 <Col><h1>Home page</h1></Col>
-                <MatrixFieldForm />
             </Row>
         )
     }
